50dbdc7e68c60c4e220b6e3deed9fc4c34e3675a
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-10-04 10:24:46 +0300

    Modified js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './modules/switch.js';
+// import './modules/switch.js';
 import './modules/date.js';
 
 const bookList = document.getElementById('book-list');
@@ -79,3 +79,27 @@ bookAdd.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
   allLibrary.getLocalStorage();
 });
+
+const SwitchPage = (pageId) => {
+  const currentPage = document.querySelector('.pages .page.is-active');
+  currentPage.classList.remove('is-active');
+
+  const nextPage = document.querySelector(`.pages .page[data-page="${pageId}"]`);
+  nextPage.classList.add('is-active');
+};
+
+window.onload = () => {
+  const tabSwitchers = document.querySelectorAll('[data-switcher]');
+
+  for (let i = 0; i < tabSwitchers.length; i += 1) {
+    const tabSwitcher = tabSwitchers[i];
+    const pageId = tabSwitcher.dataset.tab;
+
+    tabSwitcher.addEventListener('click', () => {
+      document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+      tabSwitcher.parentNode.classList.add('is-active');
+
+      SwitchPage(pageId);
+    });
+  }
+};
--- modules/switch.js
+++ modules/switch.js
@@ -21,3 +21,5 @@ window.onload = () => {
     });
   }
 };
+
+export default tabSwitcher;

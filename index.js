f39511f3de76a454863e0219af082c379d86dcb8
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-10-04 12:44:54 +0300

    Modified js

--- index.js
+++ index.js
@@ -1,75 +1,11 @@
 import './modules/date.js';
-import SwitchPage from './modules/switch.js';
+// import SwitchPage from './modules/switch.js';
 import allLibrary from './modules/class.js';
 
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookAdd = document.getElementById('book-add');
 
-//   constructor() {
-//     this.books = [
-//       {
-//         title: 'Ghost',
-//         author: 'Sidney Sheldon',
-//       },
-//       {
-//         title: 'The Naked Face',
-//         author: 'Sidney Sheldon',
-//       },
-//     ];
-//   }
-//   // add books
-
-//   addBook(title, author) {
-//     this.books.push({
-//       title,
-//       author,
-//     });
-
-//     bookAuthor.value = '';
-//     bookTitle.value = '';
-//     this.setLocalStorage();
-//     this.displayBooks();
-//   }
-
-//   removeBook(index) {
-//     this.books.splice(index, 1);
-//     this.setLocalStorage();
-//     this.displayBooks();
-//   }
-
-//   setLocalStorage() {
-//     localStorage.setItem('localLibraries', JSON.stringify(this.books));
-//   }
-
-//   getLocalStorage() {
-//     if (localStorage.getItem('localLibraries')) {
-//       this.books = JSON.parse(localStorage.getItem('localLibraries'));
-//     }
-//     this.displayBooks();
-//   }
-
-//   displayBooks() {
-//     bookList.innerHTML = '';
-//     this.books.forEach((book, index) => {
-//       const h3 = document.createElement('div');
-//       h3.classList.add('book-div');
-//       const bookNames = document.createElement('p');
-//       const removeBtn = document.createElement('button');
-//       bookNames.textContent = ` "${book.title}" by ${book.author}`;
-
-//       removeBtn.textContent = 'Remove';
-//       removeBtn.classList.add('remove');
-//       h3.append(bookNames, removeBtn);
-//       removeBtn.addEventListener('click', () => {
-//         this.removeBook(index);
-//       });
-//       bookList.appendChild(h3);
-//     });
-//   }
-// }
-
-// const allLibrary = new Booklibrary();
 bookAdd.addEventListener('click', (e) => {
   e.preventDefault();
   allLibrary.addBook(bookTitle.value, bookAuthor.value);
@@ -81,6 +17,14 @@ bookAdd.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
   allLibrary.getLocalStorage();
 });
+
+function SwitchPage(pageId) {
+  const currentPage = document.querySelector('.pages .page.is-active');
+  currentPage.classList.remove('is-active');
+
+  const nextPage = document.querySelector(`.pages .page[data-page="${pageId}"]`);
+  nextPage.classList.add('is-active');
+}
 
 window.onload = () => {
   const tabSwitchers = document.querySelectorAll('[data-switcher]');
